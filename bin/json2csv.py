ad503e7e0b62c449e20a587246bb97661b6e0faa
David F. Severski <davidski@deadheaven.com>
2014-01-09 19:27:04 -0800

    Add Unicode handling and close output file

--- bin/json2csv.py
+++ bin/json2csv.py
@@ -4,15 +4,34 @@ import simplejson as json
 import sys
 import os
 import csv
-import glob
 import re
+import glob
 
-# text file containing the full names of the fields
-fieldnames = "../keynames-real.txt"
-# the name of the output file - if exists, it will overwrite.
-outputFile = "veris.csv"
-# the filter to read in all the json files
-fileglob = "../vcdb/*.json"
+def _decode_list(data):
+    rv = []
+    for item in data:
+        if isinstance(item, unicode):
+            item = item.encode('utf-8')
+        elif isinstance(item, list):
+            item = _decode_list(item)
+        elif isinstance(item, dict):
+            item = _decode_dict(item)
+        rv.append(item)
+    return rv
+
+def _decode_dict(data):
+    rv = {}
+    for key, value in data.iteritems():
+        if isinstance(key, unicode):
+           key = key.encode('utf-8')
+        if isinstance(value, unicode):
+           value = value.encode('utf-8')
+        elif isinstance(value, list):
+           value = _decode_list(value)
+        elif isinstance(value, dict):
+           value = _decode_dict(value)
+        rv[key] = value
+    return rv
 
 def getHeader(label):
     temp = re.sub("_", " ", label)
@@ -20,12 +39,16 @@ def getHeader(label):
 
 def handledict(output, label, datadict, arraylist):
     "general function to determine how to handle value"
+    skip = [ 'partner_data', 'plus' ]
     debug = True
     if debug: print "\trunning with label: " + label
     mylist = datadict.items()
     for k,v in mylist:
         alabel = k
         if label:
+            if k in skip:
+                if debug: print "\t\tskipping label: " + k
+                continue
             alabel = ".".join([label, k])
             if (label == "actor" or label == "action" or label == "attribute"):
                 if output.get(label) is None:
@@ -75,7 +98,6 @@ def handleAny(output, label, v, arraylist):
     else:
         if debug: print "*******unknown type: ", type(v)
 
-
 def recursive(alldata, localnames):
     "Stare at this long enough and it's quite simple"
     debug = True
@@ -94,34 +116,76 @@ def recursive(alldata, localnames):
         else:
             writer.writerow(localdata)
 
+def parseSchema(v, base, mykeylist=[]):
+    "handling any single instance"
+    debug = False
+    if debug: print "ENTER:",base
+    if v['type']=="object":
+        if debug: print "trying to parse object"
+        for k,v2 in v['properties'].items():
+            if len(base):
+                callout = base + "." + k
+            else:
+                callout = k
+            if debug: print "  object calling with base of " + base
+            parseSchema(v2, callout, mykeylist)
+    elif v['type']=="array":
+        if debug: print "trying to parse array: "
+        if debug: print "  array calling with base of " + base
+        parseSchema(v['items'], base, mykeylist)
+    else:
+        if debug: print "trying to parse " + v['type']
+        mykeylist.append(base)
+    return mykeylist
 
-keyfields = []
-# F = open("keyfields-pub.txt")
+# load up the schema 
+# the full JSON schema (from github)
+verisschema = "verisc.json"
 
-F = open(fieldnames)
-rawinput = F.readlines()
-for line in rawinput:
-    foo = line.strip("\n")
-    keyfields.append(foo)
+json_schema=open(verisschema).read()
+try:
+    jschema = json.loads(json_schema)
+except:
+    print "veris schema--Unexpected error:", sys.exc_info()[1]
+
+# load up a list of all the fields expected from the schema
+keyfields = parseSchema(jschema, "")
+
+for i in keyfields:
+    its = i.split('.')
+    if its[0] == "actor" or its[0] == "action" or its[0] == "attribute":
+        # newkey = '.'.join([ its[0], its[1] ])
+        if its[0] not in keyfields:
+            keyfields.append(its[0])
+
+print keyfields
+
+#keyfields = []
+#F = open("keyfields-pub.txt")
+#rawinput = F.readlines()
+#for line in rawinput:
+#    foo = line.strip("\n")
+#    keyfields.append(foo)
 
 # print out the line here, we are iterated as much as we can be
-outfile = open(outputFile, "w")
+outfile = open("pubfact-table.csv", "w")
 writer = csv.DictWriter(outfile, fieldnames=keyfields)
-
+# headers=dict( (n,n) for n in keyfields)
 keylabels = [ getHeader(label) for label in keyfields ]
 headers = {}
 for i in range(len(keylabels)):
-    headers[keyfields[i]] = keylabels[i]
-# headers=dict( (n,n) for n in keyfields)
+        headers[keyfields[i]] = keylabels[i]
+
 writer.writerow(headers)
 
-
 # for filename in glob.glob("src2/vz_Westp-ddb-news*.json"):
-for filename in glob.glob(fileglob):
-    print filename
+for filename in glob.glob("../vcdb/data/json/*.json"): # "../github/veris/vcdb/*.json"):
+    print "************", filename, "************"
     json_data=open(filename).read()
     try:
-        data = json.loads(json_data)
+        #auto-handling unicode object hook derived from
+        #http://stackoverflow.com/questions/956867/how-to-get-string-objects-instead-unicode-ones-from-json-in-python
+        data = json.loads(json_data, object_hook=_decode_dict)
     except:
         print sys.argv[1], " Unexpected error:", sys.exc_info()[1]
     debug = True
@@ -129,8 +193,6 @@ for filename in glob.glob(fileglob):
     arraylist = {}
     handledict(output, "", data, arraylist)
     mylist = arraylist.items()
-    print "\t" + "output returned has " + str(len(output)) + " items"
-    print output
     keylist = []
     combos = 1
     for k,v in mylist:
@@ -138,5 +200,9 @@ for filename in glob.glob(fileglob):
         combos = combos * len(output[k])
 
     # print "Arrays found in",keylist
+    print "here"
     print "\t\t" + str(len(keylist)) + " combinations:",combos
     recursive(output, keylist)
+
+#clean up the open file
+outfile.close()
