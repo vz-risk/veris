a250f5c355f63217e96cdd18e3b6b63a4710f798
Kevin Thompson <kevin.david.thompson@gmail.com>
2014-03-26 23:21:33 -0500

    checkValidity1_3.py path changes
    The command line accepts a space-separated list of directories which
    overrides the newline separated values in the config file.

--- bin/checkValidity1_3.py
+++ bin/checkValidity1_3.py
@@ -94,7 +94,7 @@ if __name__ == '__main__':
     parser.add_argument("-e", "--enum", help="enumeration file to validate with", default="../verisc-enum.json")
     parser.add_argument("-l", "--logging", choices=["critical", "warning", "info"],
                         help="Minimum logging level to display", default="warning")
-    parser.add_argument("-p", "--path", help="comma-separated list of paths to search for incidents")
+    parser.add_argument("-p", "--path", nargs='+', help="list of paths to search for incidents")
     args = parser.parse_args()
     logging_remap = {'warning': logging.WARNING, 'critical': logging.CRITICAL, 'info': logging.INFO}
     logging.basicConfig(level=logging_remap[args.logging])
@@ -106,11 +106,11 @@ if __name__ == '__main__':
     config.read('checkValidity.cfg')
     data_path = []
     if args.path:
-        data_path = args.path.split(',')
+        data_path = args.path
     else:  # only use config option if nothing is specified on the command line
         try:
             path_to_parse = config.get('VERIS', 'datapath')
-            data_path = path_to_parse.split(',')
+            data_path = path_to_parse.strip().split('\n')
         except ConfigParser.Error:
             print "No path found in config file, continuing..."
             data_path = ['.']
