9b89fe08b72c516ab72da184dacdce721ad91bcf
Gabriel Bassett <gabe@infosecanalytics.com>
2020-11-25 16:14:57 -0600

    added move-up/move-down buttons to list. functionality was there but not shown due to arrayfieldtemplate

--- bin/veris_webapp/src/js/views/ArrayFieldTemplate.js
+++ bin/veris_webapp/src/js/views/ArrayFieldTemplate.js
@@ -7,6 +7,33 @@ function ArrayItem(props) {
         {props.children}
       </div>
       <div className="col-xs-2 text-right">
+
+        {(props.hasMoveUp || props.hasMoveDown) && (
+          <button
+            type="move"
+            icon="arrow-up"
+            aria-label="Move up"
+            className="btn btn-secondary btn-radius btn-move"
+            tabIndex="-1"
+            disabled={props.disabled || props.readonly || !props.hasMoveUp}
+            onClick={props.onReorderClick(props.index, props.index - 1)}
+          >Move Up</button>
+        )}
+
+        {(props.hasMoveUp || props.hasMoveDown) && (
+          <button
+            type="move"
+            icon="arrow-down"
+            className="btn btn-secondary btn-radius btn-move"
+            aria-label="Move down"
+            tabIndex="-1"
+            disabled={
+              props.disabled || props.readonly || !props.hasMoveDown
+            }
+            onClick={props.onReorderClick(props.index, props.index + 1)}
+          >Move Down</button>
+        )}
+
         {props.hasRemove && (
               <button
                 type="danger"
