d7a5aca6bd35de44968c69f3f437920f4814b8b7
Gabriel Bassett <gabe@infosecanalytics.com>
2021-11-04 09:05:32 -0500

    added webapp updated for veris 1.3.6

--- bin/veris_webapp/webpack.dev.js
+++ bin/veris_webapp/webpack.dev.js
@@ -1,12 +1,20 @@
 //const merge = require('webpack-merge');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { merge } = require('webpack-merge');
 
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-        static: './dist'
-    }
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'VERIS Webapp',
+      template: path.join(__dirname, 'assets', 'index.html')
+    })
+  ],
 });
